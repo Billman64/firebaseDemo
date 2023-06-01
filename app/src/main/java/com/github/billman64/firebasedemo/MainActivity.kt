9eaf80e8acf451a726feb89d7723ebf1b23c2395
Bill <striderbillman@gmail.com>
2023-06-01 18:55:04 -0400

    Created TextField Composable for future use (take input from user to write to database with).

--- app/src/main/java/com/github/billman64/firebasedemo/MainActivity.kt
+++ app/src/main/java/com/github/billman64/firebasedemo/MainActivity.kt
@@ -9,14 +9,24 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.github.billman64.firebasedemo.ui.theme.FirebaseDemoTheme
 import com.google.firebase.database.DataSnapshot
@@ -37,7 +47,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column{
+                        Greeting("Firebase demo")
+
+                        var textInput by remember {mutableStateOf("")}
+
+                        Row{
+                            Text("Enter value to write to db: ")
+                            BasicTextField(
+                                value = textInput, onValueChange = {textInput = it},
+                                modifier = Modifier.background(Color.Green),
+                            )
+                        }
+
+                    }
+
                 }
             }
         }
@@ -73,7 +97,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "$name",
         modifier = modifier
     )
 }
